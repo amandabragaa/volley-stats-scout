b8c5b5a3924eac8dc5621fe095744941ed1a741a
Amanda Braga <amandabraga1998@gmail.com>
2026-02-16 14:05:30 -0300

    teste

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
+import ReactLogger from "react-terminal-logger/console-logger";
 import App from "./App.tsx";
 import "./index.css";
 
+ReactLogger.start();
 createRoot(document.getElementById("root")!).render(<App />);
